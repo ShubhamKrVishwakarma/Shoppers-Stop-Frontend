14fcb2a3018d1ce1b0bfba5301208ca449d9543a
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-11-02 17:45:37 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSingup from './Pages/LoginSingup'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSingup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
