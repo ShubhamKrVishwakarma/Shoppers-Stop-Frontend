45be97dfb994e44af1306153b686f60f544b409d
Shubham Kumar Vishwakarma <vis.shubham2002@gmail.com>
2024-11-07 19:58:01 +0530

    Added Product Display Page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import "./Item.css"
+import { Link } from 'react-router-dom'
 
 const Item = (props) => {
   return (
     <div className='item'>
-      <img src={props.image} alt={props.name} />
+      <Link to={`/product/${props.id}`}>
+        <img src={props.image} alt={props.name} />
+      </Link>
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
-            &#8377; {props.new_price}
+          &#8377; {props.new_price}
         </div>
         <div className="item-price-old">
-            &#8377; {props.old_price}
+          &#8377; {props.old_price}
         </div>
       </div>
     </div>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext.jsx'
+import { useParams } from 'react-router-dom'
+import Breadcrum from '../Components/Breadcrum/Breadcrum.jsx';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay.jsx';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((item) => item.id === Number(productId));
   return (
     <div>
-      
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   )
 }
